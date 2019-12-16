c41d6d7f0dce0e3150a812c3afa087407928e792
ricsmc <Vsc231635478vsc>
2019-12-16 15:48:02 +0000

    Logout parte 2: the reckoning

--- src/server/Client.java
+++ src/server/Client.java
@@ -6,11 +6,14 @@ import java.net.Socket;
 
 public class Client {
 
+    private String username;                                 //pode ser estúpido, para o logout
+
     public void startClient() throws  IOException{
         Socket socket = new Socket("127.0.0.1", 12345);
         BufferedReader in;
         PrintWriter out;
 
+        this.username = "";
 
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream());
@@ -31,6 +34,13 @@ public class Client {
                 byte[] m = new byte[1024];
                 while(c < f.length()){
                         //i need to converter um file em binário para mandar ao server kthxbai
+                }
+            }
+
+            if(s.equals("logout")){
+                meta = s.split(" ");           //logout, username
+                if(!meta[1].equals(this.username)){
+                    //objetivo, se não der o username pedir para dar reenter
                 }
             }
 
